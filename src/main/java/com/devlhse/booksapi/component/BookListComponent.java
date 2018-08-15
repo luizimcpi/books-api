94c71a1e1adb0c5a9830edb462e03316b71d44b4
Luiz Henrique <luiz.hse@hotmail.com>
2018-08-15 14:15:24 -0300

    Adiciona Logica para pegar informações

--- src/main/java/com/devlhse/booksapi/component/BookListComponent.java
+++ src/main/java/com/devlhse/booksapi/component/BookListComponent.java
@@ -33,3 +33,49 @@ public class BookListComponent {
     }
 
 }
+
+public static final String TAG_PARAGRAPH = "p";
+//
+//	public static void main(String[] args) throws IOException {
+//		Document mainDoc = Jsoup.connect("https://kotlinlang.org/docs/books.html").get();
+//		Elements titles = mainDoc.select("h2");
+//
+//		for (Element h2 : titles){
+//			StringBuilder description = new StringBuilder();
+//			System.out.println("==========================================");
+//			System.out.println("Titulo: " + h2.text());
+//			getNextElements(h2, description);
+//			System.out.println("Description: " + description.toString());
+//		}
+//
+//	}
+//
+//
+//	private static void getNextElements(Element h2, StringBuilder description) throws IOException {
+//		Element mainNode = h2.nextElementSibling();
+//		if (mainNode != null && TAG_PARAGRAPH.equalsIgnoreCase(mainNode.nodeName())) {
+//			description.append(mainNode.text()).append("\n");
+//			getNextElements(mainNode, description);
+//		}else if(mainNode != null && mainNode.nodeName() != "h2"){
+//			if(mainNode.getElementsByClass("event-lang").text() != null && !mainNode.getElementsByClass("event-lang").text().isEmpty()){
+//				System.out.println("LANGUAGE: " + mainNode.getElementsByClass("event-lang").text());
+//			}
+//			if(mainNode.select("a").first() != null ){
+//				Element link = mainNode.select("a").first();
+//				String absHref = link.attr("abs:href");
+//				System.out.println("LINK:>>>>>>> " + absHref);
+//				String isbn = getBookIsbn(absHref);
+//				System.out.println("ISBN: " + isbn);
+//			}
+//			getNextElements(mainNode, description);
+//		}
+//	}
+//
+//	private static String getBookIsbn(String absHref) {
+//		try {
+//			Document bookInfosDoc = Jsoup.connect(absHref).get();
+//			return "Unavailable";
+//		} catch (Exception e){
+//			return "Unavailable Exception";
+//		}
+//	}
